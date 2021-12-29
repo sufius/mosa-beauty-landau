a221c9b5a63a230cf8161b4c6db5530d10f7dc39
imvlandau <itimvlandau@gmail.com>
2021-12-29 17:22:03 +0100

    add testimonials

--- src/components/DcpCarousel/DcpCarousel.tsx
+++ src/components/DcpCarousel/DcpCarousel.tsx
@@ -61,108 +61,6 @@ export default function DcpCarousel() {
                 paragraph
                 sx={{ fontSize: matchSmall ? 16 : matchMedium ? 20 : 24 }}
               >
-                Haarentfernung
-              </Typography>
-            </div>
-            <div data-swiper-parallax="-100">
-              <Typography
-                sx={{ fontSize: matchSmall ? 14 : 16 }}
-                paragraph
-                align="justify"
-              >
-                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet, vielen Dank!
-              </Typography>
-            </div>
-            <div data-swiper-parallax="-200">
-              <Typography
-                component="h5"
-                align="right"
-                sx={{ fontSize: matchSmall ? 14 : matchMedium ? 16 : 18 }}
-              >
-                - Derya G.
-              </Typography>
-            </div>
-          </Box>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Box
-            className="content"
-            sx={{ maxWidth: matchSmall ? 200 : matchMedium ? 330 : 400 }}
-          >
-            <div data-swiper-parallax="-300">
-              <Typography
-                component="h4"
-                paragraph
-                sx={{ fontSize: matchSmall ? 16 : matchMedium ? 20 : 24 }}
-              >
-                Wimpern
-              </Typography>
-            </div>
-            <div data-swiper-parallax="-100">
-              <Typography
-                sx={{ fontSize: matchSmall ? 14 : 16 }}
-                paragraph
-                align="justify"
-              >
-                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. .
-              </Typography>
-            </div>
-            <div data-swiper-parallax="-200">
-              <Typography
-                component="h5"
-                align="right"
-                sx={{ fontSize: matchSmall ? 14 : matchMedium ? 16 : 18 }}
-              >
-                - Kim N.
-              </Typography>
-            </div>
-          </Box>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Box
-            className="content"
-            sx={{ maxWidth: matchSmall ? 200 : matchMedium ? 330 : 400 }}
-          >
-            <div data-swiper-parallax="-300">
-              <Typography
-                component="h4"
-                paragraph
-                sx={{ fontSize: matchSmall ? 16 : matchMedium ? 20 : 24 }}
-              >
-                Augenbrauen
-              </Typography>
-            </div>
-            <div data-swiper-parallax="-100">
-              <Typography
-                sx={{ fontSize: matchSmall ? 14 : 16 }}
-                paragraph
-                align="justify"
-              >
-                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet!
-              </Typography>
-            </div>
-            <div data-swiper-parallax="-200">
-              <Typography
-                component="h5"
-                align="right"
-                sx={{ fontSize: matchSmall ? 14 : matchMedium ? 16 : 18 }}
-              >
-                - Anita A.
-              </Typography>
-            </div>
-          </Box>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Box
-            className="content"
-            sx={{ maxWidth: matchSmall ? 200 : matchMedium ? 330 : 400 }}
-          >
-            <div data-swiper-parallax="-300">
-              <Typography
-                component="h4"
-                paragraph
-                sx={{ fontSize: matchSmall ? 16 : matchMedium ? 20 : 24 }}
-              >
                 Gesichtsbehandlung
               </Typography>
             </div>
@@ -172,7 +70,7 @@ export default function DcpCarousel() {
                 paragraph
                 align="justify"
               >
-                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                Also das war meine erste Behandlung und ich bin so Happy 🥰🥰🥰 Du bist die Beste, werde das ab sofort regelmäßig bei dir machen. Du hast mich überzeugt, kommt selten vor, Danke 😘😘😘
               </Typography>
             </div>
             <div data-swiper-parallax="-200">
@@ -181,7 +79,110 @@ export default function DcpCarousel() {
                 align="right"
                 sx={{ fontSize: matchSmall ? 14 : matchMedium ? 16 : 18 }}
               >
-                - Ludmila P.
+                - Jasmina
+              </Typography>
+            </div>
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box
+            className="content"
+            sx={{ maxWidth: matchSmall ? 200 : matchMedium ? 330 : 400 }}
+          >
+            <div data-swiper-parallax="-300">
+              <Typography
+                component="h4"
+                paragraph
+                sx={{ fontSize: matchSmall ? 16 : matchMedium ? 20 : 24 }}
+              >
+                Lasern
+              </Typography>
+            </div>
+            <div data-swiper-parallax="-100">
+              <Typography
+                sx={{ fontSize: matchSmall ? 14 : 16 }}
+                paragraph
+                align="justify"
+              >
+              Hallo liebe samar💕 <br/>
+              Du bist nicht nur 💗 die beste in Lasern sondern auch ein toller Mensch.💗
+              </Typography>
+            </div>
+            <div data-swiper-parallax="-200">
+              <Typography
+                component="h5"
+                align="right"
+                sx={{ fontSize: matchSmall ? 14 : matchMedium ? 16 : 18 }}
+              >
+                - Husti
+              </Typography>
+            </div>
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box
+            className="content"
+            sx={{ maxWidth: matchSmall ? 200 : matchMedium ? 330 : 400 }}
+          >
+            <div data-swiper-parallax="-300">
+              <Typography
+                component="h4"
+                paragraph
+                sx={{ fontSize: matchSmall ? 16 : matchMedium ? 20 : 24 }}
+              >
+                Haarentfernung
+              </Typography>
+            </div>
+            <div data-swiper-parallax="-100">
+              <Typography
+                sx={{ fontSize: matchSmall ? 14 : 16 }}
+                paragraph
+                align="justify"
+              >
+                Ich bin zufrieden! Erste Ergebnisse nach dem ersten mal schon. 💗
+              </Typography>
+            </div>
+            <div data-swiper-parallax="-200">
+              <Typography
+                component="h5"
+                align="right"
+                sx={{ fontSize: matchSmall ? 14 : matchMedium ? 16 : 18 }}
+              >
+                - Samishk
+              </Typography>
+            </div>
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box
+            className="content"
+            sx={{ maxWidth: matchSmall ? 200 : matchMedium ? 330 : 400 }}
+          >
+            <div data-swiper-parallax="-300">
+              <Typography
+                component="h4"
+                paragraph
+                sx={{ fontSize: matchSmall ? 16 : matchMedium ? 20 : 24 }}
+              >
+                Lasern
+              </Typography>
+            </div>
+            <div data-swiper-parallax="-100">
+              <Typography
+                sx={{ fontSize: matchSmall ? 14 : 16 }}
+                paragraph
+                align="justify"
+              >
+                Beste Laserin 🌸
+              </Typography>
+            </div>
+            <div data-swiper-parallax="-200">
+              <Typography
+                component="h5"
+                align="right"
+                sx={{ fontSize: matchSmall ? 14 : matchMedium ? 16 : 18 }}
+              >
+                - Sino
               </Typography>
             </div>
           </Box>
